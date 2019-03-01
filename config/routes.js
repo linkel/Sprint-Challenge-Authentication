313489298175320abc27675d657dcac9f773d630
linkel <ironcarbide@gmail.com>
2019-03-01 08:50:37 -0800

    Set up registration function.

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -9,7 +10,26 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  if (user.username && user.password) {
+    const hash = bcrypt.hashSync(user.password, 3);
+    user.password = hash;
+    db('users').insert(user)
+    .then(result => {
+      const [id] = result;
+      db('users').where({id})
+      .first()
+      .then(userAdded => {
+        res.status(200).json(userAdded)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 }
 
 function login(req, res) {
